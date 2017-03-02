a249008cc4efef43b8b3b8cc999af7381ee5c960
Lejuez Aude <dusterherz@gmail.com>
2017-03-02 22:14:17 +0100

    Exercise 6 finished. Add study drills to the third exercise.

---
+++ ex6.py
@@ -0,0 +1,27 @@
+# left out assignment for types_of_people mentioned in inbox
+types_of_people = 10
+# change variable from 10 to types_of_people
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."  # 1st string put inside a string
+
+print(x)
+print(y)
+
+# left out f in front of string and omit extra period
+print(f"I said: {x}")  # 2nd
+# left out f in front of string and omit extra period
+print(f"I also said: '{y}'")  # 3rd
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+w = "This is the left side of ..."
+e = "a string with a right size."
+
+print(w + e)  # 4th !
+
+# change "What You Should See" snapshot to reflect changes
