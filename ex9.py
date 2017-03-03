b1451d5345f75ea814a5acbbc6f79388567a012e
Lejuez Aude <dusterherz@gmail.com>
2017-03-03 20:09:18 +0100

    Exercise 9 finished.

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's some new stange stuff, remember type it exactly.
+
+# \n is used to make a entre
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# """ """ can pe used to print on multiple lines
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
